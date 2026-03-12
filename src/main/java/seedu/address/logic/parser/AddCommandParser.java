99db32c97651c0857c062cbed6179e639c9bc3f2
desnen <tohjunyee@gmail.com>
2026-03-12 23:22:23 +0800

    Update AddCommandParser.java

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,14 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddCommand;
@@ -33,7 +28,8 @@ public class AddCommandParser implements Parser<AddCommand> {
      */
     public AddCommand parse(String args) throws ParseException {
         // ArgumentMultimap argMultimap =
-        //         ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
+        //         ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, 
+        //                 PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_TAG);
 
         // if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
         //         || !argMultimap.getPreamble().isEmpty()) {
@@ -50,6 +46,9 @@ public class AddCommandParser implements Parser<AddCommand> {
         // Temporary placeholder. Actual StudentId is assigned in AddCommand.execute().
         StudentId tempId = new StudentId("S0");
         String parts[] = args.split("\\{")[1].split(",");
+        if (parts.length() != 3) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
         Name name = ParserUtil.parseName(parts[0]);
         Phone phone = ParserUtil.parsePhone(parts[1]);
         Email email = ParserUtil.parseEmail(parts[2]);
